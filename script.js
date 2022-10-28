07825f3c8e2938be5ca5b9c56829d66a30772e2b
Monica Pereira <92680257+MonicaAPereira@users.noreply.github.com>
2022-10-27 23:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let isIgnite = true;
 function changeCard(event) {
   const card = event.currentTarget
   const bg = isIgnite 
-  ? "explorer" :"ignite"
-  isIgnite = !isIgnite
+  ? "ignite" :"explorer"
   card.style.backgroundImage = `url(/assets/bg-${bg}.svg)`
+  isIgnite = !isIgnite
 }
